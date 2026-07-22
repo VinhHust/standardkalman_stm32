d3e41b3729ab2b18329dc3952e88f71214abe7ac
Vinh <steve.vu2306@gmail.com>
2026-07-22 18:58:54 +0700

    ,,,,

---
+++ include/kalman.h
@@ -0,0 +1,35 @@
+/*
+ * kalman.h
+ *
+ *  Bo loc Kalman tuyen tinh roi rac (steady-state) cho he con lac nguoc tren xe
+ *  State:      x = [theta, theta_dot, x_cart, x_cart_dot]  (deu la bien LECH quanh upright)
+ *  Do luong:  y = [theta_lech, x_lech]                     (2 phep do: goc va vi tri cart)
+ *  Input:     u = gia toc cart lenh (m/s^2, scalar)
+ *
+ *  Gain Ld (4x2) da tinh san offline bang dlqe() trong MATLAB
+ *  -> tren chip KHONG chay nhanh hiep phuong sai P nua, chi con Correct + Predict.
+ *
+ *  Author: vu thanh vinh
+ */
+
+#ifndef KALMAN_H
+#define KALMAN_H
+
+#include <stdint.h>
+
+typedef struct {
+    float xpred[4]; //dự đoán dựa trên SSM    
+    float xcorrect[4]; //state sau khi correct 
+    uint8_t first_time; //cờ đánh dấu lần đầu chạy kalman
+} KalmanFilter;
+
+//Hàm khởi tạo ban đầu cho kalman 
+void Kalman_Firsttime(KalmanFilter *kf, float theta, float theta_dot, float x_cart, float x_cart_dot);
+
+//Hàm correct state dựa trên gain của kalman 
+void Kalman_Correct(KalmanFilter *kf, float theta_meas, float x_cart_meas);
+
+//Hàm predict state dựa trên mô hình SSM
+void Kalman_Predict(KalmanFilter *kf, float u); //nhận vào gia tốc u từ LQR
+
+#endif /* KALMAN_H */
---
+++ src/kalman.c
@@ -0,0 +1,65 @@
+#include "kalman.h"
+
+//all the matrices must be discrete
+//matrix Add, Bdd, Cdd, Ldd from matlab 
+static const float Add[4][4] = {
+    {1.0f, 0.01f, 0.0f, 0.0f},
+    {0.0f, 1.0f, 0.0f, 0.0f},
+    {0.0f, 0.0f, 1.0f, 0.01f},
+    {0.0f, 0.0f, 0.0f, 1.0f}
+};
+
+static const float Bdd[4] = {
+    0.0f,
+    0.01f,
+    0.0f,
+    0.0f
+};
+
+static const float Cdd[2][4] = {
+    {1.0f, 0.0f, 0.0f, 0.0f},
+    {0.0f, 0.0f, 1.0f, 0.0f}
+};
+
+//phan nay se thay vao cac gia tri gain Ld tinh san offline tu matlab, nen khong can tinh lai tren chip
+static const float Ldd[4][2] = {
+    {0.0, 0.0f},
+    {0.0f, 0.0f},
+    {0.0f, 0.0f},
+    {0.0f, 0.0f}
+};
+
+//trien khai noi dung trong ham kalmanfirst time 
+void Kalman_Firsttime(KalmanFilter *kf, float theta, float theta_dot, 
+                        float x_cart, float x_cart_dot) {
+//gan cac gia tri ban dau cho state cua kalman filter chinh bang gia tri do luong 
+//duoc khi kich hoat kalman filter lan dau tien
+
+                            //MEASUREMENT
+    kf->xpred[0] = theta;
+    kf->xpred[1] = theta_dot;
+    kf->xpred[2] = x_cart;
+    kf->xpred[3] = x_cart_dot;
+                            //PREDICTION
+    kf->xcorrect[0] = theta;
+    kf->xcorrect[1] = theta_dot;
+    kf->xcorrect[2] = x_cart;
+    kf->xcorrect[3] = x_cart_dot;
+//first time thi innovation = measurement - prediction = 0
+    kf->first_time = 1; //co danh dau lan chay dau tien 
+}
+
+void Kalman_Correct(KalmanFilter *kf,float delta_theta, float delta_x) {
+    //correct state
+    float innovation[2]; //innovation vector innovation 
+    innovation[0] = delta_theta - kf->xpred[0];
+    innovation[1] = delta_x - kf->xpred[2];
+
+//chi co 2 innovation nhung co the correct duoc toan bo state
+    kf->xcorrect[0] = kf->xpred[0] + Ldd[0][0]*innovation[0] + Ldd[0][1]*innovation[1];
+    kf->xcorrect[1] = kf->xpred[1] + Ldd[1][0]*innovation[0] + Ldd[1][1]*innovation[1];
+    kf->xcorrect[2] = kf->xpred[2] + Ldd[2][0]*innovation[0] + Ldd[2][1]*innovation[1];
+    kf->xcorrect[3] = kf->xpred[3] + Ldd[3][0]*innovation[0] + Ldd[3][1]*innovation[1];
+
+    kf->first_time = 0; //da chay qua lan dau tien roi
+}
